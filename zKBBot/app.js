1a7c50640040487d770298cbd9fc99f9616850cc
Sean Kimball <smkimbal@gmail.com>
2017-06-17 08:38:06 -0700

    Working v1 with logs

--- zKBBot/app.js
+++ zKBBot/app.js
@@ -5,36 +5,42 @@ const settings = require('./settings.json');
 
 function blah(message) {
 	request.get('https://redisq.zkillboard.com/listen.php', (error, response, body) => {
-		var test = JSON.parse(body)
-		var attackerCount = 0;
-		var wasVictim = false;
-		// console.log(test.package.killmail.attackers);
-		console.log('--------------------------------------------------------')
-		console.log(test.package.killmail.victim);
-		if (test.package.killmail) {
-			test.package.killmail.attackers.forEach(function(element) {
-				if (element.character === undefined) {
-					console.log('We do not need this shit');
-				} else {
-					attackerCount++;
-				}
-			}, this);
+		var test;
+		if (body) {
+			test = JSON.parse(body);
+			var attackerCount = 0;
+			var wasVictim = false;
+			// console.log(test);
+			// console.log('--------------------------------------------------------')
+			// console.log(test.package.killmail.attackers);
+			if (test.package) {
+				test.package.killmail.attackers.forEach(function(element) {
+					if (element.character) {
+						console.log(element.corporation.name);
+						if (element.corporation.name === 'ArcJet Heavy Industries') {
+							console.log('Attacker was an ArcJet member');
+							attackerCount++;
+						}
+					}
+				});
 
-			if (test.package.killmail.victim.character) {
-				console.log('Victim was a real character')
-				if (test.package.killmail.victim.corporation === 'ArcJet Heavy Industries') {
-					wasVictim = true;
+				if (test.package.killmail.victim.character) {
+					if (test.package.killmail.victim.corporation === 'ArcJet Heavy Industries') {
+						console.log('Victim was an ArcJet member');
+						wasVictim = true;
+					}
 				}
 			}
+			console.log('Attackers that were ArcJet: ' + attackerCount);
+			if (attackerCount > 0 || wasVictim) {
+				const killUrl = 'https://zkillboard.com/kill/' + test.package.killID;
+				message.channel.send(killUrl);
+			}
+			// var body = 'https://zkillboard.com/kill/' + test.package.killID
+			// bluh(body)
+			// console.log(body.data)
 		}
-		if (attackerCount > 0 || wasVictim) {
-			const killUrl = 'https://zkillboard.com/kill/' + test.package.killID;
-			message.channel.send(killUrl);
-		}
-		// var body = 'https://zkillboard.com/kill/' + test.package.killID
-		// bluh(body)
-		// console.log(body.data)
-	})
+	});
 }
 
 client.on('ready', () => {
